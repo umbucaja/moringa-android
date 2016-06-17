bec90d78f36a3822c6fe7e6e2cb54780fba29e32
Jordao Serafim <jordaoesa@gmail.com>
2016-06-17 14:07:42 -0300

    teste

--- app/src/main/java/umbucaja/moringa/fragments/SobreFragment.java
+++ app/src/main/java/umbucaja/moringa/fragments/SobreFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -60,6 +61,8 @@ public class SobreFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
