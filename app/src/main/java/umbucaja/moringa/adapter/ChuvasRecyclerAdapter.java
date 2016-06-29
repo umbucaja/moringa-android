835f5c914b60f22fb0891801f2e04c3a60f6e952
Jordao Serafim <jordaoesa@gmail.com>
2016-06-29 11:25:10 -0300

    reza a lenda q se o servico for implementado, os cards de chuva vao funcionar

--- app/src/main/java/umbucaja/moringa/adapter/ChuvasRecyclerAdapter.java
+++ app/src/main/java/umbucaja/moringa/adapter/ChuvasRecyclerAdapter.java
@@ -4,11 +4,18 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import umbucaja.moringa.R;
 import umbucaja.moringa.entity.MeasurementStation;
+import umbucaja.moringa.entity.RainFallMeasurement;
 
 /**
  * Created by jordaoesa on 28/06/2016.
@@ -30,7 +37,39 @@ public class ChuvasRecyclerAdapter extends RecyclerView.Adapter<ChuvasRecyclerAd
 
     @Override
     public void onBindViewHolder(ChuvasHolder holder, int position) {
+        MeasurementStation station = stations.get(position);
+        holder.tvNomeEstacao.setText(station.getName());
+        List<RainFallMeasurement> measurements = station.getRainFallMeasurements();
+        if(measurements != null && measurements.size() > 0){
+            Collections.sort(measurements, new Comparator<RainFallMeasurement>() {
+                @Override
+                public int compare(RainFallMeasurement rfm1, RainFallMeasurement rfm2) {
+                    return rfm1.getDate().compareTo(rfm2.getDate());
+                }
+            });
+            RainFallMeasurement lastMeasurement = measurements.get(measurements.size()-1);
 
+            DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+            String date = formatter.format(lastMeasurement.getDate());
+            Float value = lastMeasurement.getValue();
+            String unit = lastMeasurement.getUnit();
+
+            holder.tvMilimetragem.setText(String.format("%.1f%s",value,unit));
+            holder.tvData.setText(date);
+
+            checkValuesAndSetImage(value, holder);
+
+        }
+    }
+
+    //TODO: precisamos das imagens e dos intervalos para terminar a implementacao!
+    private void checkValuesAndSetImage(Float value, ChuvasHolder holder) {
+        /*if(value <= 10)
+            holder.ivSituacao.setImageDrawable();
+        else if(value <= 50)
+            holder.ivSituacao.setImageDrawable();
+        else
+            holder.ivSituacao.setImageDrawable();*/
     }
 
     @Override
@@ -39,8 +78,18 @@ public class ChuvasRecyclerAdapter extends RecyclerView.Adapter<ChuvasRecyclerAd
     }
 
     public static class ChuvasHolder extends RecyclerView.ViewHolder{
+
+        public ImageView ivSituacao;
+        public TextView tvMilimetragem;
+        public TextView tvNomeEstacao;
+        public TextView tvData;
+
         public ChuvasHolder(View itemView) {
             super(itemView);
+            ivSituacao = (ImageView) itemView.findViewById(R.id.image_view_chuvas);
+            tvMilimetragem = (TextView) itemView.findViewById(R.id.tv_chuvas_milimetragem);
+            tvNomeEstacao = (TextView) itemView.findViewById(R.id.tv_chuvas_station_name);
+            tvData = (TextView) itemView.findViewById(R.id.tv_chuvas_last_measurement_date);
         }
     }
 }
