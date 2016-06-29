827440679a06484639773fa981865662a0c3cf47
Romeryto <romeryto.lira@splab.ufcg.edu.br>
2016-06-29 16:51:19 -0300

    versao atual acudes

--- app/src/main/java/umbucaja/moringa/adapter/WaterSourceRecyclerAdapter.java
+++ app/src/main/java/umbucaja/moringa/adapter/WaterSourceRecyclerAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -43,6 +44,7 @@ public class WaterSourceRecyclerAdapter extends RecyclerView.Adapter<WaterSource
     @Override
     public void onBindViewHolder(WaterSourceViewHolder holder, int position) {
         WaterSource waterSource = waterSources.get(position);
+        Toast.makeText(holder.context, waterSource.getName(), Toast.LENGTH_LONG).show();
         holder.tvName.setText(waterSource.getType()+" "+waterSource.getName());
         float capacity = waterSource.getCapacity();
         List<WaterSourceMeasurement> wsms = waterSource.getReservoirMeasurements();
@@ -50,18 +52,18 @@ public class WaterSourceRecyclerAdapter extends RecyclerView.Adapter<WaterSource
 
         float actualVolume = 0;
         String date = "";
+        holder.currentWaterSource = waterSource;
         if(wsms!=null){
             if(wsms.size()>0){
-                actualVolume = wsms.get(0).getValue();
+                actualVolume = wsms.get(wsms.size()-1).getValue();
                 DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
                 date = formatter.format(wsms.get(wsms.size()-1).getDate());
+
             }else{
 
             }
-
         }
         percentage = (actualVolume*100)/capacity;
-
 
         holder.tvPercentage.setText(String.format("%.1f%s",percentage,"%"));
         holder.tvDate.setText(date);
@@ -74,6 +76,7 @@ public class WaterSourceRecyclerAdapter extends RecyclerView.Adapter<WaterSource
     public int getItemCount() {
         return waterSources.size();
     }
+
 
 
 }
--- app/src/main/java/umbucaja/moringa/adapter/WaterSourceViewHolder.java
+++ app/src/main/java/umbucaja/moringa/adapter/WaterSourceViewHolder.java
@@ -1,31 +1,54 @@
 package umbucaja.moringa.adapter;
 
 import android.content.Context;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import umbucaja.moringa.R;
+import umbucaja.moringa.entity.WaterSource;
+import umbucaja.moringa.fragments.WaterSourceFragment;
 
 /**
  * Created by Romeryto on 21/06/2016.
  */
-public class WaterSourceViewHolder extends RecyclerView.ViewHolder {
+public class WaterSourceViewHolder extends RecyclerView.ViewHolder implements WaterSourceViewHolderInterface, View.OnClickListener {
 
     public Context context;
     public TextView tvName;
     public TextView tvPercentage;
     public TextView tvDate;
     public ProgressBar progressBarPercentage;
+    public WaterSource currentWaterSource;
 
 
-    public WaterSourceViewHolder(Context context, View itemView){
+    public WaterSourceViewHolder(Context context, View itemView) {
         super(itemView);
         this.context = context;
         tvName = (TextView) itemView.findViewById(R.id.tv_water_source_name);
         tvPercentage = (TextView) itemView.findViewById(R.id.tv_water_source_percentage);
         tvDate = (TextView) itemView.findViewById(R.id.tv_water_source_last_measurement_date);
         progressBarPercentage = (ProgressBar) itemView.findViewById(R.id.progress_bar_id);
+
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Toast.makeText(view.getContext(), currentWaterSource.getName(), Toast.LENGTH_LONG).show();
+        Class fragmentClass = WaterSourceFragment.class;
+        Fragment fragment = null;
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //FragmentManager fragmentManager = view.getSupportFragmentManager();
+       // fragmentManager.beginTransaction().replace(R.id.fragmentView, fragment).commit();
     }
 }
